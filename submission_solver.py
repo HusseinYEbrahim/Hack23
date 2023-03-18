256e58196ad79982af114f46c751d928b530d4cc
HusseinYEbrahim <127052130+HusseinYEbrahim@users.noreply.github.com>
2023-03-18 02:41:54 -0700

    Add files via upload

--- submission_solver.py
+++ submission_solver.py
@@ -111,6 +111,8 @@ def submission_inference(riddle_solvers, mp, orientation):
 
     response = requests.post(f'http://{server_ip}:5000/init', json={"agentId": agent_id})
 
+    print(response.text())
+
 
     obv = get_obv_from_response(response)
 
